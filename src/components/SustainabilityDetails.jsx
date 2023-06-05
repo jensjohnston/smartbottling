870eebcbaf437399cf03cc0768469a3ec5263419
jensjohnston <johnstonjr27@gmail.com>
2023-06-05 17:26:52 +0200

    add frame and firt animation

--- src/components/SustainabilityDetails.jsx
+++ src/components/SustainabilityDetails.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
+import { motion, useTransform } from 'framer-motion';
+import { useScroll } from 'framer-motion';
+
 import feature1 from '@/images/background1.jpg'
 import mineral from '@/images/mineral.png'
 import deliciousWater from '@/images/delicious-bluewater-water.png'
@@ -9,22 +12,8 @@ import { Recycle } from '@/components/Recycle'
 
 
 export function SustainabilityDetails() {
-  // Replace this with your actual data
-  const features = [
-    {
-      title: 'Sustainability Defined',
-      description: "Squash plastic waste, be the change.",
-      media: feature1,
-      alignment: 'left'
-    },
-    {
-      title: 'Efficiency, Redefined',
-      description: "Curb expenses, boost operations",
-      media: "/images/feature-image2.jpg", // Path to your image
-      alignment: 'right'
-    },
-    // ... add more features here if needed
-  ];
+  const { scrollYProgress } = useScroll();
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 900]);
 
   return (
     <section className="py-20 lg:pb-96 pt-0 lg:pt-20">
@@ -32,11 +21,17 @@ export function SustainabilityDetails() {
             <div className="flex flex-col items-center justify-center">
                 <div className="max-w-6xl lg:pb-6 px-4">
                     <h2 className="text-left text-4xl leading-tight md:leading-none md:text-8xl font-bold break-words lg:text-center">Sustainability <br /> Defined</h2>
-                    <p className="text-left text-2xl max-w-2xl mt-4 font-medium text-gray-500 lg:text-center">Stand at the vanguard of the <span className="text-gray-900">green revolution</span>. With SmartBottlingplant™, you&apos;re not just reducing plastic waste. You&apos;re actively shaping a <span className="text-gray-900">sustainable future</span>, embodying the ethos of <span className="text-gray-900">eco-luxury</span>.</p>
+                    <p className="text-left text-2xl max-w-2xl mt-4 font-medium text-gray-500 lg:text-center z-20 relative">Stand at the vanguard of the <span className="text-gray-900">green revolution</span>. With SmartBottlingplant™, you&apos;re not just reducing plastic waste. You&apos;re actively shaping a <span className="text-gray-900">sustainable future</span>, embodying the ethos of <span className="text-gray-900">eco-luxury</span>.</p>
                 </div>
                 <div className="max-w-screen-md pt-8 lg:pt-20 relative">
-                    <div className="flex px-4">
-                    <Recycle className="h-auto w-full" />
+                    <div className="flex px-4 max-w-screen-md mx-auto">
+                      <motion.div
+                        className="h-auto w-full max-w-screen-md overflow-hidden"
+                        style={{ rotate }}
+                        transition={{ duration: 0.5 }}
+                      >
+                        <Recycle />
+                      </motion.div>
                     </div>
                     <div className="absolute -right-1/4 sm:-right-2/4 transform top-1/2 -translate-y-1/2 -mt-96 hidden md:block">
                         <Image src={mineral} className="w-40 lg:w-80" alt="" />
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,11 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ParallaxProvider>
+      <Component {...pageProps} />
+    </ParallaxProvider>
+  )
 }
