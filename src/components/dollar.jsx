a62871b0d412e7b7345d8cf3287c0b51d583d139
jensjohnston <johnstonjr27@gmail.com>
2023-06-06 11:42:41 +0200

    change blue color on all svgs

--- src/components/dollar.jsx
+++ src/components/dollar.jsx
@@ -4,8 +4,8 @@ return (
     <g clip-path="url(#clip0_9416_7906)">
         <path
             d="M121.18 53.8346C121.18 51.4999 120.926 40.9725 112.691 32.61C102.885 22.6344 89.7256 23.823 87.2211 24.1201C72.0243 25.8605 57.3793 39.3169 57.5066 53.8346C57.634 71.8755 80.3443 82.3604 82.9762 83.549C96.8995 89.789 104.031 84.6527 112.691 92.0388C120.459 98.7034 124.322 111.014 121.18 121.753C116.808 136.568 100.38 143.827 87.2211 142.978C70.8781 141.917 56.2756 128.036 53.2617 109.019"
-            stroke="#0092CB" stroke-width="12.7348" stroke-miterlimit="10" stroke-linecap="round" />
-        <path d="M87.2266 11.3843V164.201" stroke="#0092CB" stroke-width="12.7348" stroke-miterlimit="10"
+            stroke="#3B82F6" stroke-width="12.7348" stroke-miterlimit="10" stroke-linecap="round" />
+        <path d="M87.2266 11.3843V164.201" stroke="#3B82F6" stroke-width="12.7348" stroke-miterlimit="10"
             stroke-linecap="round" />
     </g>
     <defs>
